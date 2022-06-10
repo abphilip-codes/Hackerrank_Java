64b8c85f50b89528c90b881e247b4bd088e65586
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-10 10:03:40 +0530

    programs

---
+++ 25.java
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/java-string-tokens/problem
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        if (s.length() > 0) {
+            String[] t = s.split("[!,?._'@\\s]+");
+            System.out.println(t.length);
+            for(String z : t) {System.out.println(z);}
+        } 
+        else {System.out.println(0);}
+        scan.close();
+    }
+}
