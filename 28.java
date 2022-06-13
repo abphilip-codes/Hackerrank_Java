bcc3c9d7e9c9ab81a5f626192093a4395125933e
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-13 08:10:02 +0530

    programs

---
+++ 28.java
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/valid-username-checker/problem
+
+import java.util.Scanner;
+
+class UsernameValidator {
+    public static final String regularExpression = "(^[a-zA-Z][0-9_|a-zA-Z]{7,29})";
+}
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else { System.out.println("Invalid"); }           
+        }
+    }
+}
