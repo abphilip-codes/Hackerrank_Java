98ca08e4356b98414811fe1854db0eab2872bb4a
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-15 10:50:13 +0530

    programs

---
+++ 30.java
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/java-bigdecimal/problem
+
+import java.math.BigDecimal;
+import java.util.*;
+class Solution {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String []s = new String[n+2];
+        for(int i=0; i<n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
+
+        s[n] = "Nope";
+        s[n+1] = "Nope";
+        Arrays.sort(s,((String number1, String number2) ->  {
+            if(!(number1.equals("Nope")) && !(number2.equals("Nope"))) {
+                BigDecimal value1 = new BigDecimal(number1);
+                BigDecimal value2 = new BigDecimal(number2);
+                return value2.compareTo(value1);
+            }
+            return 0;
+        }));
+
+        for(int i=0; i<n; i++) { System.out.println(s[i]); }
+    }
+}
