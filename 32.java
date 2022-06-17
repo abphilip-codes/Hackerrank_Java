9bf5190bf6cc2577390d9a0d28be0868b7f3c299
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-17 11:04:41 +0530

    programs

--- 32.java
+++ 32.java
@@ -1,22 +1,17 @@
-// https://www.hackerrank.com/challenges/java-primality-test/problem
+// https://www.hackerrank.com/challenges/java-biginteger/problem
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
-        BigInteger n = in.nextBigInteger();
-        System.out.println(n.isProbablePrime(1) ? "prime" :"not prime");
-        in.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        java.math.BigInteger ai = sc.nextBigInteger();
+        java.math.BigInteger bi = sc.nextBigInteger();
+        java.math.BigInteger sum = ai.add(bi);
+        java.math.BigInteger mul = ai.multiply(bi);
+        System.out.println(sum); 
+        System.out.println(mul);
+        sc.close();
     }
 }
