99e950fd374fa2d7ad7c1f6ac9ac8ff687b90324
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-24 13:43:12 +0530

    programs

--- 36.java
+++ 36.java
@@ -6,18 +6,21 @@ import java.io.*;
 class Solution{
 	public static void main(String []argh)
 	{
-		Scanner in = new Scanner(System.in);
-		int n=in.nextInt();
-		in.nextLine();
-		for(int i=0;i<n;i++)
-		{
-			String name=in.nextLine();
-			int phone=in.nextInt();
-			in.nextLine();
-		}
-		while(in.hasNext())
-		{
-			String s=in.nextLine();
-		}
+      	Scanner in = new Scanner(System.in);
+      	int n = in.nextInt();
+      	in.nextLine();
+
+      	HashMap<String,Integer> h = new HashMap<>();
+      	for(int i=0; i<n; i++) {
+         	String name = in.nextLine();
+         	int phone = in.nextInt();
+         	h.put(name, phone);
+         	in.nextLine();
+     	}
+      	while(in.hasNext())	{
+         	String s = in.nextLine();
+         	if(h.get(s)!=null) { System.out.println(s+"="+h.get(s)); }
+         	else { System.out.println("Not found"); }
+      	}
 	}
 }
