9a6f27a87dea9b7081365869065644af795788f5
Allen Ben Philipose <allenalvin333@gmail.com>
2022-06-30 17:14:54 +0530

    [rograms

---
+++ 42.java
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/java-dequeue/problem
+
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
+        
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            int input = in.nextInt();
+            deque.add(input);
+            set.add(input);
+            
+            if(deque.size() == m) {
+                if(set.size() > max) 
+                    max = set.size();
+                int a = deque.remove();
+                if(!deque.contains(a)) 
+                    set.remove(a);
+            }
+        }
+        System.out.println(max);
+        in.close();
+    }
+}
