3b49f69a5eac53a305b800a98cf5d5934fecd52f
Allen Ben Philipose <allenalvin333@gmail.com>
2022-07-02 14:20:35 +0530

    programs

--- 43.java
+++ 43.java
@@ -1,31 +1,37 @@
-// https://www.hackerrank.com/challenges/java-bitset/problem
+// https://www.hackerrank.com/challenges/java-b/problem
 
+import java.io.*;
 import java.util.*;
 
-public class test {
+public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Deque<Integer> deque = new ArrayDeque<>();
-        HashSet<Integer> set = new HashSet<>();
-        
         int n = in.nextInt();
         int m = in.nextInt();
-        int max = Integer.MIN_VALUE;
-
-        for(int i=0; i<n; i++) {
-            int input = in.nextInt();
-            deque.add(input);
-            set.add(input);
-            
-            if(deque.size() == m) {
-                if(set.size() > max) 
-                    max = set.size();
-                int a = deque.remove();
-                if(!deque.contains(a)) 
-                    set.remove(a);
+        BitSet b1 = new BitSet(n);
+        BitSet b2 = new BitSet(n);
+        BitSet[] b = new BitSet[3];
+        b[1] = b1; b[2] = b2;
+      
+        while(m-- >0) {
+            String s = in.next();
+            int x = in.nextInt();
+            int y = in.nextInt();
+          
+            switch(s) {
+                case "AND":
+                    b[x].and(b[y]); break;
+                case "OR":
+                    b[x].or(b[y]); break;
+                case "XOR":
+                    b[x].xor(b[y]); break;
+                case "FLIP":
+                    b[x].flip(y); break;
+                case "SET":
+                    b[x].set(y);
             }
+            System.out.printf("%d %d%n", b1.cardinality(), b2.cardinality());
         }
-        System.out.println(max);
         in.close();
     }
 }
