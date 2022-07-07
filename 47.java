49ce03924a53df1cf982ec8eb6aac3f30010ea73
Allen Ben Philipose <allenalvin333@gmail.com>
2022-07-07 07:49:17 +0530

    programs

---
+++ 47.java
@@ -0,0 +1,24 @@
+// https://www.hackerrank.com/challenges//java-abstract-class/problem
+
+import java.util.*;
+abstract class Book {
+	String title;
+	abstract void setTitle(String s);
+	String getTitle() { return title; }
+}
+
+class MyBook extends Book {
+    @Override
+    void setTitle(String s) { title = s; }
+}
+
+public class Main {
+	public static void main(String []args){
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      	sc.close();
+	}
+}
