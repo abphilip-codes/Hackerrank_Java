b21b6f15f10caae6c771eabcd1d61f8b30931da5
Allen Ben Philipose <allenalvin333@gmail.com>
2022-11-22 10:21:14 +0530

    Java End-of-file

--- Introduction_09.java
+++ Introduction_09.java
@@ -5,10 +5,9 @@ import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    for(int i = 1; scan.hasNext()== true; i++){
-         System.out.println(i + " " + scan.nextLine());
-   }
+        Scanner scan = new Scanner(System.in);
+        for(int i = 1; scan.hasNext()== true; i++){
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
 }
-}
-
