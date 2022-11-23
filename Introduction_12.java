625e096305a192f462a8f280cf0c9a7da403c4a3
Allen Ben Philipose <allenalvin333@gmail.com>
2022-11-23 09:02:24 +0530

    Java Date and Time

--- Introduction_12.java
+++ Introduction_12.java
@@ -24,20 +24,14 @@ public class Solution {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
         int month = Integer.parseInt(firstMultipleInput[0]);
-
         int day = Integer.parseInt(firstMultipleInput[1]);
-
         int year = Integer.parseInt(firstMultipleInput[2]);
 
         String res = Result.findDay(month, day, year);
-
         bufferedWriter.write(res);
         bufferedWriter.newLine();
-
         bufferedReader.close();
         bufferedWriter.close();
     }
 }
-
