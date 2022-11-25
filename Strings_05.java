8538ea4b1011447eac09c438296df105bcaa074d
Allen Ben Philipose <allenalvin333@gmail.com>
2022-11-25 09:37:40 +0530

    Java Anagrams

--- Strings_05.java
+++ Strings_05.java
@@ -3,11 +3,10 @@
 import java.util.Scanner;
 import java.util.regex.*;
 public class Solution {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int c = Integer.parseInt(in.nextLine());
-        while(c>0)
-        {
+        while(c>0) {
             String p = in.nextLine();
             try 
             {
